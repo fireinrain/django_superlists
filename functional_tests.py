0895cfdbc0c5c969d73b4f21a0ba66f0c3793732
lzhaoyang <575563079@qq.com>
2016-12-19 14:27:39 +0800

    完成了前端页面从模板生成的基本过程

--- functional_tests.py
+++ functional_tests.py
@@ -46,7 +46,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows)
+            any(row.text == '1:Buy peacock feathers' for row in rows),
+            "new to-do item did not appear in table"
         )
 
         # 页面有显示了一个文本框，可以输入其它待办事项
