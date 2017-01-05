fe1d6655058772a534351578c2f6e0bac5601c08
lzhaoyang <575563079@qq.com>
2017-01-05 13:43:21 +0800

    重构测试

--- lists/tests.py
+++ lists/tests.py
@@ -151,7 +151,35 @@ class ListViewTest(TestCase):
             response,'other list item 2'
         )
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
 
+        self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data={'item_text':'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(
+            new_item.text,'A new item for an existing list'
+        )
+        self.assertEqual(
+            new_item.list,correct_list
+        )
+
+    def test_POST_redirect_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertRedirects(
+            response,'/lists/%d/' % (correct_list.id,)
+        )
 # 单元测试--数据模型测试
 class ListAndItemModelTest(TestCase):
 
